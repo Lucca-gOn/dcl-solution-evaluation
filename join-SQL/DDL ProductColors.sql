694f7ff94e513946f49a98aaeba763fb1ab8e8d8
unknown <lucas.gon.oliv@gmail.com>
2024-04-08 18:21:06 -0300

    Adicionado DDL do BD ProductColors

---
+++ join-SQL/DDL ProductColors.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE ProductColors;
+
+USE ProductColors;
+
+CREATE TABLE Produto(
+	IdProduto INT PRIMARY KEY IDENTITY,
+	NomeProduto VARCHAR(50),
+	PrecoProduto DECIMAL(10, 2)
+);
+
+CREATE TABLE Cor(
+	IdCor INT PRIMARY KEY IDENTITY,
+	IdProduto INT FOREIGN KEY REFERENCES Produto(IdProduto),
+	NomeCor VARCHAR(50),
+	PrecoCor DECIMAL(10, 2),
+);
